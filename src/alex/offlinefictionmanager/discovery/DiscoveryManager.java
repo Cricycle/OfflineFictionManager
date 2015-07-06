28e9e9d1b5cc377ea0d7ec620845b39c5d186730
Alex Anderson <alexanderson@ufl.edu>
2015-07-05 18:56:48 -0700

    Discover stories with Jsoup
    Using Jsoup library, we look at author pages and story pages on fanfiction.net
    From author pages, we find their stories and their favorite stories.
    From story pages, we find how many more chapters there are for the story.
    
    Ideally, we should not load the story page, but instead the author page when trying
    to get information about a story, so that we minimize screwing up the view counts for authors.
    
    Currently we will print out the discovered stories and chapters as a list.

--- src/alex/offlinefictionmanager/discovery/DiscoveryManager.java
+++ src/alex/offlinefictionmanager/discovery/DiscoveryManager.java
@@ -5,7 +5,10 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.reflect.Field;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Scanner;
 
 import javax.xml.XMLConstants;
 import javax.xml.parsers.DocumentBuilder;
@@ -22,6 +25,7 @@ import javax.xml.xpath.XPathExpression;
 import javax.xml.xpath.XPathExpressionException;
 import javax.xml.xpath.XPathFactory;
 
+import org.jsoup.Jsoup;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
@@ -34,8 +38,10 @@ import org.xml.sax.SAXException;
  */
 public class DiscoveryManager
 {
-	public static String ConfigurationFile = "discovery_config.xml";
-	public static String ConfigurationSchemaFile = "discovery_config.xsd";
+	private static String ConfigurationFile = "discovery_config.xml";
+	private static String ConfigurationSchemaFile = "discovery_config.xsd";
+	private static String StartingPointTempResource = "temporary_source.txt";
+	private static String UserAgent = "Offline Fiction Manager v0 (cricycle.stackexchange@gmail.com)";
 
 	/**
 	 * The maximum number of Internet bound requests we will do per minute.
@@ -51,6 +57,11 @@ public class DiscoveryManager
 	@LoadFromXml
 	private int maxDiscoverLimitPerSession;
 
+	private List<String> newSources;
+	private List<Story> existingStories;
+	private HashMap<String, String> discoveredStories;
+	private HashMap<String, Story> newChapters;
+
 	public static void main(String[] args)
 	{
 		// Initial plan:
@@ -61,16 +72,30 @@ public class DiscoveryManager
 		// download requests
 
 		DiscoveryManager manager = new DiscoveryManager();
-		manager.LoadConfiguration();
+		manager.LoadStartingPoint();
 		manager.Discover();
 		manager.RequestDownload();
 
 		System.out.println(manager.maxRequestsPerMinuteRate);
 		System.out.println(manager.maxDiscoverLimitPerSession);
+
+		for (String storyName : manager.discoveredStories.keySet())
+		{
+			System.out.printf("Found story: NAME=%s, URL=%s\n", storyName, manager.discoveredStories.get(storyName));
+		}
+
+		System.out.println();
+
+		for (String chapterURL : manager.newChapters.keySet())
+		{
+			System.out.printf("Found new chapter: URL=%s    Story=%s\n", chapterURL,
+				manager.newChapters.get(chapterURL).storyName);
+		}
 	}
 
 	public DiscoveryManager()
 	{
+		LoadConfiguration();
 	}
 
 	public void LoadConfiguration()
@@ -129,7 +154,6 @@ public class DiscoveryManager
 			LogError("Unknown critical error when loading configuration for Discovery Manager.", e);
 			throw new RuntimeException(e);
 		}
-
 	}
 
 	private void getDiscoveryManagerProperties(Document doc, HashMap<String, String> nameValueMap,
@@ -158,11 +182,122 @@ public class DiscoveryManager
 		}
 	}
 
+	/**
+	 * Loads what sites to look at for new stories/chapters
+	 */
+	public void LoadStartingPoint()
+	{
+		this.newSources = new ArrayList<String>();
+		this.existingStories = new ArrayList<Story>();
+
+		Scanner scanner = new Scanner(this.getClass().getResourceAsStream(DiscoveryManager.StartingPointTempResource));
+		int numSiteSources = scanner.nextInt();
+		scanner.nextLine();
+		for (int i = 0; i < numSiteSources; ++i)
+		{
+			String site = scanner.nextLine(); // read whole line in case of spaces in URL
+			this.newSources.add(site);
+			// TODO log sources
+		}
+
+		int numKnownStories = scanner.nextInt();
+		scanner.nextLine();
+		for (int i = 0; i < numKnownStories; ++i)
+		{
+			String storyName = scanner.nextLine();
+			String storySite = scanner.nextLine();
+			int knownChapterCount = Integer.parseInt(scanner.nextLine());
+			this.existingStories.add(new Story(storyName, storySite, knownChapterCount));
+			// TODO: Log loading existing stories count
+		}
+	}
+
+	/**
+	 * Looks at sites for new stories/chapters
+	 * Requires that LoadStartingPoint() has run
+	 * Results in a list of websites, along with a reason for why we target it
+	 */
 	public void Discover()
 	{
+		DiscoverStories();
+		DiscoverChapters();
+	}
+
+	/**
+	 * Go through site sources for stories (author sites on FF.net)
+	 */
+	private void DiscoverStories()
+	{
+		for (String authorSite : this.newSources)
+		{
+			try
+			{
+				org.jsoup.nodes.Document doc = Jsoup.connect(authorSite).userAgent(DiscoveryManager.UserAgent).get();
+				this.discoveredStories = new HashMap<>();
+
+				// Look for stories by the user first
+				org.jsoup.select.Elements authoredStories = doc.getElementsByClass("mystories");
+				for (org.jsoup.nodes.Element e : authoredStories)
+				{
+					String betterStoryName = e.getElementsByClass("stitle").first().text();
+					String storyURL = e.getElementsByClass("stitle").first().attr("abs:href");
+					this.discoveredStories.put(betterStoryName, storyURL);
+				}
+
+				// Look at the user's favorite stories next
+				org.jsoup.select.Elements favoritedStories = doc.getElementsByClass("favstories");
+				for (org.jsoup.nodes.Element e : favoritedStories)
+				{
+					String betterStoryName = e.getElementsByClass("stitle").first().text();
+					String storyURL = e.getElementsByClass("stitle").first().attr("abs:href");
+					this.discoveredStories.put(betterStoryName, storyURL);
+				}
+			}
+			catch (IOException e)
+			{
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+
+	/**
+	 * Given a link to some chapter in a story, + count of how many chapters already loaded, load more chaps
+	 */
+	private void DiscoverChapters()
+	{
+		for (Story knownStory : this.existingStories)
+		{
+			try
+			{
+				org.jsoup.nodes.Document doc = Jsoup.connect(knownStory.getURL()).userAgent(DiscoveryManager.UserAgent)
+					.get();
+
+				this.newChapters = new HashMap<String, Story>();
+
+				// Look at the chapter selection and get the chapter count
+				org.jsoup.select.Elements optionTags = doc.select("select").first().select("option");
+				for (org.jsoup.nodes.Element e : optionTags)
+				{
+					int chapterNum = Integer.parseInt(e.attr("value"));
+					if (chapterNum > knownStory.knownChapterCount)
+					{
+						this.newChapters.put(knownStory.getURL(chapterNum), knownStory);
+					}
+				}
+			}
+			catch (IOException e)
+			{
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 
 	}
 
+	/**
+	 * Passes list of discovered sites to download new stories/chapters
+	 */
 	public void RequestDownload()
 	{
 
@@ -179,4 +314,28 @@ public class DiscoveryManager
 @Retention(RetentionPolicy.RUNTIME)
 @interface LoadFromXml
 {
+}
+
+class Story
+{
+	String storyName;
+	String websiteURL;
+	int knownChapterCount;
+
+	public Story(String storyName, String websiteURL, int knownChapterCount)
+	{
+		this.storyName = storyName;
+		this.websiteURL = websiteURL;
+		this.knownChapterCount = knownChapterCount;
+	}
+
+	public String getURL()
+	{
+		return getURL(1);
+	}
+
+	public String getURL(int chapNum)
+	{
+		return String.format(websiteURL, chapNum);
+	}
 }
